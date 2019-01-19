2b56e8cb1185cc34f43a2c65b47c2051f25a295b
DragonKiller115 <41318456+DragonKiller115@users.noreply.github.com>
2019-01-19 03:07:47 -0600

    Count Finalizer
    Adding a new script for stop a count coroutine.

---
+++ Unity Scripts/Finalizer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finalizer : MonoBehaviour
+{
+    private bool finalizer = false;
+    private bool comprobation = false;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Press method for stop the count.
+        Press();
+        //Comprobation for count execution.
+        if(!comprobation)
+        {
+            comprobation = true;
+            StartCoroutine(Count());           
+        }
+    }
+
+    void Press()
+    {
+        if(Input.GetKey(KeyCode.F))
+        {
+            Debug.Log("Method stoped<Count>");
+            finalizer = true;
+        }
+    }
+    IEnumerator Count()
+    {
+        if(!finalizer)
+        {
+            for(int i = 1; i != 0; i++)
+            {
+                Debug.Log("Segundos: " + i + "s");
+                yield return new WaitForSeconds(1f);
+                //Comprobation for break the count
+                if(finalizer)
+                {break;}
+            }
+        } 
+    }
+}
